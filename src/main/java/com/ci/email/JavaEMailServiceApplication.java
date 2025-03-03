288b701ab49290cbfcf5b9262f85fd313c8c3200
ratnendrr.girri <ratnendrr@chandikaInnov.com>
2025-03-03 13:33:49 +0530

    base files added

---
+++ src/main/java/com/ci/email/JavaEMailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ci.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEMailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEMailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ci/email/JavaEMailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEMailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
